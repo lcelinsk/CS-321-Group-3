464c082fb8a5a02a508959a482acc5743176cb0d
lcelinsk <lcelinsk@gmu.edu>
2024-03-19 20:57:29 -0400

    Attorney and Form commit
    First commit for the Attorney and Form classes

---
+++ Attorney.java
@@ -0,0 +1,32 @@
+public class Attorney{
+	//Class Attributes
+	private String fullName = "";
+	private String BarID = "";
+	private String state = "";
+	
+	//Getter methods for the Attorney class
+	public String getFullName(){
+		return this.fullName;
+	}
+	
+	public String getBarID(){
+		return this.BarID;
+	}
+	
+	public String getState(){
+		return this.state;
+	}
+	
+	//Setter methods for Attorney class
+	public void setFullName(String fullName){
+		this.fullName = fullName;
+	}
+	
+	public void setBarID(String BarID){
+		this.BarID = BarID;
+	}
+	
+	public void setState(String state){
+		this.state = state;
+	}
+}
---
+++ Form.java
@@ -0,0 +1,68 @@
+public class Form{
+	//Class attributes
+	private boolean requireAttorney = false;
+	private Attorney attorney = null;
+	private String legalAssistance = "";
+	private String formCorrection = "";
+	private boolean approved = false;
+	private String immigrantEmail = "";
+	private String ID = "";
+	
+	//Getter methods for all Form attributes
+	public boolean getRequireAttorney(){
+		return this.requireAttorney;
+	}
+	
+	public Attorney getAttorney(){
+		return this.attorney;
+	}
+	
+	public String getLegalAssistance(){
+		return this.legalAssistance;
+	}
+	
+	public String getFormCorrection(){
+		return this.formCorrection;
+	}
+	
+	public boolean getApproved(){
+		return this.approved;
+	}
+	
+	public String getImmigrantEmail(){
+		return this.immigrantEmail;
+	}
+	
+	public String getID(){
+		return ID;
+	}
+	
+	//Setter methods for all Form attributes
+	public void setRequireAttorney(String requireAttorney){
+		this.requireAttorney = requireAttorney;
+	}
+	
+	public void setAttorney(Attorney attorney){
+		this.attorney = attorney;
+	}
+	
+	public void setLegalAssistance(String legalAssistance){
+		this.legalAssistance = legalAssistance;
+	}
+	
+	public void setFormCorrection(String formCorrection){
+		this.formCorrection = formCorrection;
+	}
+	
+	public void setApproved(boolean approved){
+		this.approved = approved;
+	}
+	
+	public void setImmigrantEmail(String immigrantEmail){
+		this.immigrantEmail = immigrantEmail;
+	}
+	
+	public void setID(String ID){
+		this.ID = ID;
+	}
+}
